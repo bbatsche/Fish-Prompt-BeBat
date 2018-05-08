7cc1c5e69285f3ff33f6cc1269554a5cb85cead2
Ben Batschelet <bbatschelet@magento.com>
2018-05-08 15:09:30 -0500

    Maybe one day we'll recreate / fix HG prompt.

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -20,34 +20,5 @@ set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 set -g __fish_git_prompt_color_cleanstate -o green
 
 function fish_right_prompt
-    if hg id > /dev/null ^ /dev/null
-        set hg_clean (set_color -o green)"✔"
-        set hg_status (set_color magenta)(hg prompt "{update}{status|modified|unknown}" ^ /dev/null)
-        set hg_outgoing (set_color cyan)(hg prompt "{↑{outgoing|count}}" ^ /dev/null)
-        set hg_incoming (set_color blue)(hg prompt "{↓{incoming|count}}" ^ /dev/null)
-
-        echo -n -s $__fish_prompt_normal "("
-
-        set_color white
-
-        hg prompt "{branch}" ^ /dev/null
-
-        echo -n -s $__fish_prompt_normal ":"
-
-        if [ $hg_status ]
-            echo -n -s $hg_status
-        else
-            echo -n -s $hg_clean
-
-            if test -n "$hg_outgoing" -o -n "$hg_incoming"
-                echo -n " "
-            end
-        end
-
-        echo -n -s $hg_outgoing $hg_incoming
-
-        echo -n -s $__fish_prompt_normal ")"
-    end
-
     echo -n -s (__fish_git_prompt)
 end
